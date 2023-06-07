fecfff1162b2cb663c61c40372daf42359de8433
Francis <franco.gianello@sstrategy.io>
2023-06-07 13:30:51 +0200

    Ajustes de estilos de css del index, implementada barra de busqueda de campeones

--- tfg/league/static/league/js/index.js
+++ tfg/league/static/league/js/index.js
@@ -1,13 +1,14 @@
-function championKey() {
+
+function championSearch() {
     //array de elementos en los que buscar
-    let list_champions = document.getElementsByClassName("list_champions")
+    let championList = document.getElementsByClassName("champion")
     let input = document.getElementById("championKey");
     //valor del input
     let filter = input.value.toUpperCase();
-    for (let i = 0; i < list_champions.length; i++) {
-        //summoner name
-        let champion = list_champions[i].getElementsByClassName("champions")[0];
+    for (let i = 0; i < championList.length; i++) {
+        //champion name
+        let champion = championList[i].getElementsByTagName("input")[0].value;
         //comparar si existe en cada uno de estos
-        list_champions.innerHTML += champion; 
+        championList[i].style.display = (champion.toUpperCase().indexOf(filter) > -1) ? "" : "none"; 
     }
 }
