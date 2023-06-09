503c4bad255f134e749bbf90d6af2d199b0424b9
Francis <franco.gianello@sstrategy.io>
2023-06-09 16:44:19 +0200

    Grafica implementada

--- tfg/league/static/league/js/detail_champion.js
+++ tfg/league/static/league/js/detail_champion.js
@@ -1,5 +1,7 @@
 window.onload = function () {
     let data = document.getElementsByClassName("data-chart")
+    console.log(data);
+    console.log(data[0].getElementsByTagName("input")[0].value);
     var chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
         theme: "dark1", // "light1", "light2", "dark1", "dark2"
@@ -15,10 +17,10 @@ window.onload = function () {
             legendMarkerColor: "grey",
             legendText: "1 bloque",
             dataPoints: [      
-                { y: data[0].getElementsByTagName("input").value, label: "Defensa" },
-                { y: 4,  label: "Saudi" },
-                { y: 3,  label: "Canada" },
-                { y: 6,  label: "Iran" },
+                { y: Number(data[0].getElementsByTagName("input")[0].value), label: "Ataque" },
+                { y: Number(data[0].getElementsByTagName("input")[1].value),  label: "Defensa" },
+                { y: Number(data[0].getElementsByTagName("input")[2].value),  label: "Magia" },
+                { y: Number(data[0].getElementsByTagName("input")[3].value),  label: "Dificultad" },
             ]
         }]
     });
