23b89a3c0588eed4b752dc21840e374186b0d481
Nataly <natalymt03@gmail.com>
2023-06-11 11:14:42 +0200

    loader y otros done

--- tfg/league/static/league/js/base.js
+++ tfg/league/static/league/js/base.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loader = document.getElementById("loader");
     loader.style.display = "block";
+    
   
     window.addEventListener("load", function () {
       loader.style.display = "none";
+      const header = document.getElementsByTagName("header");
+    header.style.display= "none";
+    const main = document.getElementsByTagName("main");
+    main.style.display= "none";
+    const footer = document.getElementsByTagName("footer");
+    footer.style.display= "none";
     });
+
+
   });
