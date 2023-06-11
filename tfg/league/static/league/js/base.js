0d34698c87c0609742c06ffeef1bf430d5fdb32e
Nataly <natalymt03@gmail.com>
2023-06-11 10:36:02 +0200

    footer y otros done

---
+++ tfg/league/static/league/js/base.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loader = document.getElementById("loader");
+    loader.style.display = "block";
+  
+    window.addEventListener("load", function () {
+      loader.style.display = "none";
+    });
+  });
